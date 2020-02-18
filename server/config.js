962687e22b07c8934b9115071c47363697f07633
DeOne <deone@deone.ru>
2020-02-18 20:26:05 +0300

    Add config

---
+++ server/config.js
@@ -0,0 +1,18 @@
+const db = {
+  url:
+    'mongodb://battleship:pvGmmZu21FwfEidr@cluster0-ohatu.mongodb.net/battleship?retryWrites=true&w=majority&authSource=admin',
+}
+
+const bcrypt = {
+  saltRounds: 10,
+}
+
+const jwt = {
+  secret: 'jT%F8LHy@Loy0HV%3ELZn*0hQNGr7oKAiIyiZoWzdIoh5tapf!',
+}
+
+module.exports = {
+  db,
+  bcrypt,
+  jwt,
+}
