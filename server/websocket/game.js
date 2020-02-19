8475a3fc9c4058e34047f54180a423892b19d01b
98o718 <kalyakin.maks@mail.ru>
2020-02-19 20:27:12 +0300

    Implement game start

--- server/websocket/game.js
+++ server/websocket/game.js
@@ -1,9 +1,37 @@
 const { isUuid } = require('uuidv4')
 
 module.exports = io => {
+  const users = {}
+
+  const games = {}
+
   io.sockets.on('connection', socket => {
-    socket.on('kek', () => {
-      console.log(socket.id)
+    users[socket.id] = {
+      game: null,
+      player: null,
+      room: null,
+      ships: null,
+    }
+
+    socket.on('ready', payload => {
+      const { room } = users[socket.id]
+      console.log('players in room before push:', games[room].players)
+      games[room].players.push(socket.id)
+      console.log('room', room)
+
+      const readyPlayers = games[room].players.length
+
+      console.log('players in room:', games[room].players)
+      console.log('players ready:', readyPlayers)
+
+      users[socket.id].player = readyPlayers - 1
+      users[socket.id].ships = payload.ships
+
+      io.to(socket.id).emit('player', readyPlayers - 1)
+
+      if (readyPlayers === 2) {
+        io.to(room).emit('start', games[room].turn)
+      }
     })
 
     socket.on('room', room => {
@@ -13,6 +41,15 @@ module.exports = io => {
           io.sockets.adapter.rooms[room].length < 2)
       ) {
         socket.join(room)
+        users[socket.id].room = room
+
+        if (!games[room]) {
+          games[room] = {
+            players: [],
+            turn: Math.floor(Math.random() * 2),
+          }
+        }
+
         console.log(socket.id)
         console.log('joined room', room)
         console.log(
