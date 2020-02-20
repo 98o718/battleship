7e8edee127f84e039060b9f4388cbb1e18b36bdd
98o718 <kalyakin.maks@mail.ru>
2020-02-20 19:49:20 +0300

    Connect start and play in room pages

--- client/src/pages/Start/Start.jsx
+++ client/src/pages/Start/Start.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Logo, Button } from '../../components'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 import {
   StartWrapper,
   Wave,
@@ -15,30 +15,42 @@ import {
   LinkContainer,
 } from './Start.styles'
 
+import { constants } from '../../constants'
+
 import authImg from '../../img/authImg.png'
 import wave from '../../img/wave.svg'
 import macaroni from '../../img/macaroniLogo.png'
 import bomb from '../../img/bomb.png'
 import ferry from '../../img/ferry.png'
 
-export const Start = props => (
-  <StartWrapper>
-    <Logo></Logo>
-    <Auth>
-      <AuthImg src={authImg} />
-      <LinkContainer>
-        <Link to="/auth">Войти</Link> | <Link to="/auth">Регистрация</Link>
-      </LinkContainer>
-    </Auth>
-    <GameMenu>
-      <Text>Выбор режима</Text>
-      <Button state="start" text="Случайная игра" />
-      <Line />
-      <Button state="start" text="Игра с другом" />
-      <Macaroni src={macaroni} alt="macaroni logo" />
-    </GameMenu>
-    <Bomb src={bomb} alt="bomb" />
-    <Ferry src={ferry} />
-    <Wave src={wave} alt="wave" />
-  </StartWrapper>
-)
+export const Start = () => {
+  const [, setLocation] = useLocation()
+
+  const handleNewRoom = () => {
+    fetch(constants.GENERATE_ROOM_URL)
+      .then(r => r.text())
+      .then(room => setLocation(`${constants.GAME_URL}/${room}`))
+  }
+
+  return (
+    <StartWrapper>
+      <Logo></Logo>
+      <Auth>
+        <AuthImg src={authImg} />
+        <LinkContainer>
+          <Link to="/auth">Войти</Link> | <Link to="/auth">Регистрация</Link>
+        </LinkContainer>
+      </Auth>
+      <GameMenu>
+        <Text>Выбор режима</Text>
+        <Button state="start" text="Случайная игра" />
+        <Line />
+        <Button onClick={handleNewRoom} state="start" text="Игра с другом" />
+        <Macaroni src={macaroni} alt="macaroni logo" />
+      </GameMenu>
+      <Bomb src={bomb} alt="bomb" />
+      <Ferry src={ferry} />
+      <Wave src={wave} alt="wave" />
+    </StartWrapper>
+  )
+}
