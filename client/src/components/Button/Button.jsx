0bfe128c2b9fb23cbd60c6dee2727e140af921ed
98o718 <kalyakin.maks@mail.ru>
2020-02-20 19:47:38 +0300

    Fix button props

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { ButtonWrapper } from './Button.styles'
 
 const Button = props => (
-  <ButtonWrapper state={props.state}>{props.text}</ButtonWrapper>
+  <ButtonWrapper {...props} state={props.state}>
+    {props.text}
+  </ButtonWrapper>
 )
 export default Button
