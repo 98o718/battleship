246b4c74d5baebb56b94b34274a11e13cc4ae828
98o718 <kalyakin.maks@mail.ru>
2020-02-20 19:32:13 +0300

    Add game implementation

--- server/websocket/game.js
+++ server/websocket/game.js
@@ -11,26 +11,87 @@ module.exports = io => {
       player: null,
       room: null,
       ships: null,
+      flatShips: null,
     }
+
+    socket.on('disconnect', () => {
+      console.log(socket.id, 'disconnect')
+      const { room } = users[socket.id]
+      delete games[room]
+      delete users[socket.id]
+
+      socket.emit('leave')
+    })
 
     socket.on('ready', payload => {
       const { room } = users[socket.id]
-      console.log('players in room before push:', games[room].players)
       games[room].players.push(socket.id)
       console.log('room', room)
 
       const readyPlayers = games[room].players.length
 
       console.log('players in room:', games[room].players)
-      console.log('players ready:', readyPlayers)
 
       users[socket.id].player = readyPlayers - 1
-      users[socket.id].ships = payload.ships
+      users[socket.id].ships = payload.ships.map((ship, idx) => {
+        return {
+          id: idx,
+          coords: ship,
+          size: ship.length,
+          hits: 0,
+        }
+      })
+      users[socket.id].flatShips = payload.ships.flat()
 
       io.to(socket.id).emit('player', readyPlayers - 1)
 
       if (readyPlayers === 2) {
         io.to(room).emit('start', games[room].turn)
+      }
+    })
+
+    socket.on('shot', shot => {
+      const { room } = users[socket.id]
+      const { players, turn } = games[room]
+
+      console.log('shot')
+
+      if (socket.id !== players[turn]) {
+        // если чужая очередь
+      } else {
+        const opponent = players[(turn + 1) % 2]
+
+        let hit = false
+        let willBeDeleted = false
+
+        users[opponent].ships.forEach(ship => {
+          ship.coords.forEach(coord => {
+            if (coord.x === shot.x && coord.y === shot.y) {
+              hit = true
+              ship.hits += 1
+              if (ship.hits === ship.size) willBeDeleted = ship.id
+            }
+          })
+        })
+
+        games[room].turn = hit ? turn : (turn + 1) % 2
+
+        io.to(room).emit('result', {
+          turn: hit ? turn : (turn + 1) % 2,
+          shot,
+          hit,
+        })
+
+        if (typeof willBeDeleted === 'number') {
+          users[opponent].ships = users[opponent].ships.filter(
+            ship => ship.id !== willBeDeleted
+          )
+          io.to(room).emit('killed', willBeDeleted)
+        }
+
+        if (users[opponent].ships.length === 0) {
+          io.to(room).emit('gameover', turn)
+        }
       }
     })
 
