c6a2e75cc5e882f46a2d69835cf7d00ef75db040
Artem Vydro <iriqtu@gmail.com>
2020-02-20 14:16:43 +0300

    Add start page

---
+++ client/src/components/Logo/Logo.styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const LogoWrapper = styled.div`
+  position: fixed;
+  top: 2rem;
+  left: 2rem;
+`
+export const LogoImage = styled.img`
+  width: 30rem;
+`
--- client/src/pages/Start/Start.jsx
+++ client/src/pages/Start/Start.jsx
@@ -1,4 +1,44 @@
 import React from 'react'
-//import { Test } from './Start.styles';
+import { Logo, Button } from '../../components'
+import { Link } from 'wouter'
+import {
+  StartWrapper,
+  Wave,
+  GameMenu,
+  Macaroni,
+  Bomb,
+  Ferry,
+  Text,
+  Line,
+  Auth,
+  AuthImg,
+  LinkContainer,
+} from './Start.styles'
 
-export const Start = props => <div className="StartWrapper">Test content</div>
+import authImg from '../../img/authImg.png'
+import wave from '../../img/wave.svg'
+import macaroni from '../../img/macaroniLogo.png'
+import bomb from '../../img/bomb.png'
+import ferry from '../../img/ferry.png'
+
+export const Start = props => (
+  <StartWrapper>
+    <Logo></Logo>
+    <Auth>
+      <AuthImg src={authImg} />
+      <LinkContainer>
+        <Link to="/auth">Войти</Link> | <Link to="/auth">Регистрация</Link>
+      </LinkContainer>
+    </Auth>
+    <GameMenu>
+      <Text>Выбор режима</Text>
+      <Button state="start" text="Случайная игра" />
+      <Line />
+      <Button state="start" text="Игра с другом" />
+      <Macaroni src={macaroni} alt="macaroni logo" />
+    </GameMenu>
+    <Bomb src={bomb} alt="bomb" />
+    <Ferry src={ferry} />
+    <Wave src={wave} alt="wave" />
+  </StartWrapper>
+)
