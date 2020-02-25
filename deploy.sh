f4f5123cc9afa35d34e06e47dbe7f1c3086a7b77
98o718 <kalyakin.maks@mail.ru>
2020-02-26 00:08:53 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,7 @@ yarn
 CI=false yarn build
 rsync -rq --delete --rsync-path="mkdir -p battleship-client && rsync" \
 $TRAVIS_BUILD_DIR/client/build/ travis@51.15.124.103:battleship-client
+npm install -g pm2
+cd ../server
+pm2 deploy ecosystem.config.js production --force
+
