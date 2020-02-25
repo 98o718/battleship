004d011b63573a4893eaf2eed34ec242eceb0980
98o718 <kalyakin.maks@mail.ru>
2020-02-25 13:10:06 +0300

    Style toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route } from 'wouter'
 import { Global, css } from '@emotion/core'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import emotionNormalize from 'emotion-normalize'
 
 import { Game, Start, WaitingRoom } from './pages'
@@ -19,9 +21,17 @@ export const App = () => {
             min-height: 100%;
             font-family: Helvetica, Arial, sans-serif;
           }
+
+          .Toastify__toast {
+            border-radius: 5px;
+
+            &--success {
+              background: #4caf50;
+            }
+          }
         `}
       />
-
+      <ToastContainer />
       <Route path="/waiting-room" component={WaitingRoom} />
       <Route path="/game/:room" component={Game} />
       <Route path="/" extends component={Start} />
