6318be46ecd1308f2865b505f806c5f05fe02839
98o718 <kalyakin.maks@mail.ru>
2020-02-25 22:29:22 +0300

    Update travis config

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,6 @@ rsync -rq --delete --rsync-path="mkdir -p battleship-client && rsync" \
 $TRAVIS_BUILD_DIR/client/build/ travis@51.15.124.103:battleship-client
 npm install -g pm2
 cd ../server
+pm2 deploy production setup
 pm2 deploy ecosystem.config.js production --force
 
