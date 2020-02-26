0332a3ecfaec923b06ac9b3e386e118436eb0cff
DeOne <deone@deone.ru>
2020-02-26 22:15:15 +0300

    Add stat model and routes

--- server/routes/stat.js
+++ server/routes/stat.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const router = express.Router()
 
-const { updateRating } = require('./../services/statService')
-
 const User = require('./../models/user')
+const Stat = require('./../models/stat')
 
-router.get('/', async (req, res) => {})
+router.get('/', async (req, res) => {
+  const { id } = res.locals.auth
+
+  Stat.find({ $or: [{ winner: id }, { loser: id }] }, (err, stat) => {
+    if (err || !stat) return res.json({ status: true, stat: [] })
+    else {
+      const stats = []
+      stat.forEach(value => {
+        stats.push({
+          id: value.id,
+          winner: value.winner.username,
+          loser: value.loser.username,
+          createdAt: value.createdAt,
+        })
+      })
+
+      return res.json({ status: true, stat: stats })
+    }
+  })
+    .populate('winner')
+    .populate('loser')
+})
+
+router.get('/counts', async (req, res) => {
+  const { id } = res.locals.auth
+
+  const wins = await Stat.find({ winner: id }).countDocuments()
+  const loss = await Stat.find({ loser: id }).countDocuments()
+
+  return res.json({ wins, loss })
+})
 
 router.get('/rating', async (req, res) => {
   const { id } = res.locals.auth
