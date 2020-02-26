0177c78c7c05d03c187fa095bb6c8a115aa66001
DeOne <deone@deone.ru>
2020-02-26 21:24:45 +0300

    Added elo rank

---
+++ server/routes/stat.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const { updateRating } = require('./../services/statService')
+
+router.get('/', async (req, res) => {})
+
+module.exports = router
