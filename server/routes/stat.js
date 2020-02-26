cccc2bc3cd99b205b46b329a8e23e2d91c544579
DeOne <deone@deone.ru>
2020-02-26 21:36:38 +0300

    Add route stat rating

--- server/routes/stat.js
+++ server/routes/stat.js
@@ -3,6 +3,19 @@ const router = express.Router()
 
 const { updateRating } = require('./../services/statService')
 
+const User = require('./../models/user')
+
 router.get('/', async (req, res) => {})
+
+router.get('/rating', async (req, res) => {
+  const { id } = res.locals.auth
+
+  User.findById(id, (err, user) => {
+    if (err || !user)
+      return res.json({ status: false, message: 'Пользователь не найден' })
+
+    return res.json({ status: true, rating: user.rating })
+  })
+})
 
 module.exports = router
