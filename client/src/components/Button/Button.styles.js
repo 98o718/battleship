e7abfd11d47e0e25fb554cf9b3803206d3e9c2e7
Artem Vydro <iriqtu@gmail.com>
2020-02-27 14:03:34 +0300

    Update buttons

--- client/src/components/Button/Button.styles.js
+++ client/src/components/Button/Button.styles.js
@@ -11,17 +11,7 @@ const start = css`
   border-radius: 10px;
   min-width: fit-content;
   transition: 0.14s;
-  cursor: pointer;
-  text-transform: uppercase;
   font-size: 18pt;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  &:hover {
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
-  &:focus {
-    outline: none;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
 `
 
 const ready = css`
@@ -33,17 +23,7 @@ const ready = css`
   padding: 1rem;
   border-radius: 10px;
   transition: 0.14s;
-  cursor: pointer;
-  text-transform: uppercase;
   font-size: 12pt;
-  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.19), 0 3px 3px rgba(0, 0, 0, 0.23);
-  &:hover {
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
-  &:focus {
-    outline: none;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
 `
 
 const common = css`
@@ -55,22 +35,28 @@ const common = css`
   padding: 1rem;
   border-radius: 10px;
   transition: 0.14s;
-  cursor: pointer;
-  text-transform: uppercase;
   font-size: 12pt;
-  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.19), 0 3px 3px rgba(0, 0, 0, 0.23);
-  &:hover {
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
-  &:focus {
-    outline: none;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
 `
 
 const defaultState = css`
   background-color: blue;
   color: white;
+`
+
+const stateForAll = css`
+  cursor: pointer;
+  text-transform: uppercase;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.19), 0 3px 3px rgba(0, 0, 0, 0.23);
+  &:hover {
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+  &:focus {
+    outline: none;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+  &:active {
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
 `
 export const ButtonWrapper = styled.button(props => {
   switch (props.state) {
@@ -83,4 +69,4 @@ export const ButtonWrapper = styled.button(props => {
     default:
       return defaultState
   }
-})
+}, stateForAll)
