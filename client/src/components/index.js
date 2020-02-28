226c00b4cfd1268365f1cb18dee35270a0a1d3ef
98o718 <kalyakin.maks@mail.ru>
2020-02-28 14:10:15 +0300

    Add playfield components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,8 @@
 /*APPEND COMPONENT IMPORT HERE*/
+import Ship from './Ship'
+import ShipPicker from './ShipPicker'
+import Field from './Field'
+import Cell from './Cell'
 import PlayField from './PlayField'
 import Logo from './Logo'
 import Button from './Button'
@@ -6,6 +10,10 @@ import EndGame from './EndGame'
 
 export {
   /*APPEND COMPONENT EXPORT HERE*/
+  Ship,
+  ShipPicker,
+  Field,
+  Cell,
   PlayField,
   Logo,
   Button,
