62448a05df95a7fad93256e6843bf4478198537f
Artem Vydro <iriqtu@gmail.com>
2020-02-29 15:44:15 +0300

    Add disabled state to button

--- client/src/components/Button/Button.styles.js
+++ client/src/components/Button/Button.styles.js
@@ -68,6 +68,11 @@ const stateForAll = css`
   &:active {
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   }
+  &:disabled {
+    cursor: default;
+    opacity: 0.4;
+    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.19), 0 3px 3px rgba(0, 0, 0, 0.23);
+  }
 `
 export const ButtonWrapper = styled.button(props => {
   switch (props.state) {
