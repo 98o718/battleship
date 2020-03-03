1103273d2d2080b5676f628f7284ec13a4e76ebc
98o718 <kalyakin.maks@mail.ru>
2020-03-03 15:14:08 +0300

    Fix email regexp

--- client/src/components/Authorization/Authorization.jsx
+++ client/src/components/Authorization/Authorization.jsx
@@ -25,7 +25,7 @@ const Authorization = props => {
   const validate = () => {
     const { email, password } = credentials
 
-    const emailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const emailCheck = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     if (!email || !emailCheck.test(email)) {
       toast.error('Введите корректный email!')
