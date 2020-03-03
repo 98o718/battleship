542225e8045e78314474b729166a92ad6db8e98f
98o718 <kalyakin.maks@mail.ru>
2020-03-03 18:29:16 +0300

    Use unsubcribing atoms workaround

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from '@reatom/core'
+import { Provider } from '@reatom/react'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(JSON.parse(`${localStorage.getItem('app_store')}`))
+
+ReactDOM.render(
+  <Provider value={store}>
+    <App store={store} />
+  </Provider>,
+  document.getElementById('root')
+)
