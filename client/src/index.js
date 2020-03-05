ed51d3986df54ab9d528c7a78c7f3e21b561a976
98o718 <kalyakin.maks@mail.ru>
2020-03-05 20:01:56 +0300

    Add regular waiting room by default

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom'
 import { createStore } from '@reatom/core'
 import { Provider } from '@reatom/react'
 
+import { gameTypes } from './constants'
+
 import { App } from './App'
 
-const store = createStore(JSON.parse(`${localStorage.getItem('app_store')}`))
+const store = createStore(
+  Object.assign({}, JSON.parse(`${localStorage.getItem('app_store')}`), {
+    gameType: gameTypes.REGULAR,
+  })
+)
 
 ReactDOM.render(
   <Provider value={store}>
